1c7409b644bf28942ea975fafde84da2ac71bc4f
Aleksejs Pavlovs <aleksejspa@lmt.lv>
2021-06-03 13:40:20 +0300

    .
    .

--- depo/index.php
+++ depo/index.php
@@ -2,54 +2,32 @@
 include ("controller.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<style>
-		
-		body{
-			width:60%;
-			margin:auto;
-			padding-top:200px;
-		}
-
-	</style>
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<title>Document</title>
+  <title>Register</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	
+ <form id="register_form">
+      <h1>Register</h1>
+      <div id="error_msg"></div>
+	  <div>
+	 	<input type="text" name="username" placeholder="Username" id="username" >
+	    <span></span>
+	  </div>
+	  <div>
+	    <input type="email" name="email" placeholder="Email" id="email">
+		<span></span>
+	  </div>
+	  <div>
+	   <input type="password" name="password" placeholder="Password" id="password">
+	  </div>
+	  <div>
+	 	<button type="button" name="register" id="reg_btn">Register</button>
+	  </div>
+	</form>
 </body>
 </html>
-<form method="POST">
-  <div class="form-group">
-    <label for="email">Email:</label>
-    <input type="email" class="form-control" id="email" placeholder="Enter username" name="uname" required>
-
-  </div>
-  <div class="form-group">
-    <label for="password">Password:</label>
-    <input type="password" class="form-control" id="password" placeholder="Enter password" name="pswd" required>
-  </div>
-  <div class="form-group">
-    <label for="password_confirm">Retype password:</label>
-    <input type="password" class="form-control" id="password_conf" placeholder="Enter password" name="pswd" required>
-  </div>
-  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-</form>
+<!-- scripts -->
+<script src="jquery-3.6.0.min.js"></script>
 <script src="app.js"></script>
-</body>
-</html>
-
-	
